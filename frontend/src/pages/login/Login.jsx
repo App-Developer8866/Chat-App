191c3c100835e7293e5bf90c90739fbfc013b307
muttaraf <muttaraf@dpskw.com>
2024-09-02 20:15:42 +0500

    Design login and sign up page on frontend and set background image

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const eyePath = showPassword
+    ? 'M15 12c0 1.654-1.346 3-3 3s-3-1.346-3-3 1.346-3 3-3 3 1.346 3 3zm9-.449s-4.252 8.449-11.985 8.449c-7.18 0-12.015-8.449-12.015-8.449s4.446-7.551 12.015-7.551c7.694 0 11.985 7.551 11.985 7.551zm-7 .449c0-2.757-2.243-5-5-5s-5 2.243-5 5 2.243 5 5 5 5-2.243 5-5z'
+    : 'm17.069 6.546 2.684-2.359c.143-.125.32-.187.497-.187.418 0 .75.34.75.75 0 .207-.086.414-.254.562l-16.5 14.501c-.142.126-.319.187-.496.187-.415 0-.75-.334-.75-.75 0-.207.086-.414.253-.562l2.438-2.143c-1.414-1.132-2.627-2.552-3.547-4.028-.096-.159-.144-.338-.144-.517s.049-.358.145-.517c2.111-3.39 5.775-6.483 9.853-6.483 1.815 0 3.536.593 5.071 1.546zm2.318 1.83c.967.943 1.804 2.013 2.475 3.117.092.156.138.332.138.507s-.046.351-.138.507c-2.068 3.403-5.721 6.493-9.864 6.493-1.298 0-2.553-.313-3.73-.849l2.624-2.307c.352.102.724.156 1.108.156 2.208 0 4-1.792 4-4 0-.206-.016-.408-.046-.606zm-4.932.467c-.678-.528-1.53-.843-2.455-.843-2.208 0-4 1.792-4 4 0 .741.202 1.435.553 2.03l1.16-1.019c-.137-.31-.213-.651-.213-1.011 0-1.38 1.12-2.5 2.5-2.5.474 0 .918.132 1.296.362z';
+
+  return (
+    <div className="card glass w-96">
+      <div className="card-body flex flex-col">
+        <h1 className="card-title text-3xl font-semibold  justify-center items-center">
+          Login
+        </h1>
+
+        <form className="mb-5">
+          <div>
+            <div className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </div>
+            <label className="input input-bordered flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+              </svg>
+              <input type="text" className="grow" placeholder="Username" />
+            </label>
+          </div>
+
+          <div>
+            <div className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </div>
+            <label className="input input-bordered flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                className="grow"
+                placeholder="Password"
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70 cursor-pointer"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                <path fillRule="evenodd" d={eyePath} clipRule="evenodd" />
+              </svg>
+            </label>
+          </div>
+          <div className="flex justify-end mt-2">
+            <a className="link link-hover">Forgot Password?</a>
+          </div>
+        </form>
+        <div className="card-actions justify-between items-center">
+          <a className="link link-hover">Don't have account</a>
+          <button className="btn">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
